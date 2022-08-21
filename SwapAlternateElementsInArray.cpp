b3df810c681e856a55c9a534bd801e95444d7255
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2022-08-21 08:47:29 +0530

    Update SwapAlternateElementsInArray.cpp

--- SwapAlternateElementsInArray.cpp
+++ SwapAlternateElementsInArray.cpp
@@ -13,7 +13,7 @@ int main() {
 
     for (int i = 0; i < n; i+=2) 
     {
-
+        // we can also use swap(arr[i],arr[i+1])
         temp = arr[i];
         arr[i] = arr[i+1];
         arr[i+1] = temp;
