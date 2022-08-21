7a04757eef468077adbfdb2b6cdf326bd6fe25f4
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2022-08-21 08:46:49 +0530

    Add files via upload

---
+++ SwapAlternateElementsInArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,temp;
+    cin >> n;
+
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i+=2) 
+    {
+
+        temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        
+    } // if n = 8 last swap will occur at index 6 cause 6+2 will be 8 and 8 < 8 is false
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+
+}
